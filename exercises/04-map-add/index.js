85482bd889dc8406b505a7f48aa399c4d2322009
BJ Neilsen <bj.neilsen@gmail.com>
2017-11-15 15:24:01 -0600

    Fix names

--- exercises/04-map-add/index.js
+++ exercises/04-map-add/index.js
@@ -1,10 +1,4 @@
-// const add = (left, right) => left + right
+const add = (left, right) => left + right
 
-// // How can we eliminate defining a new function in map using partial application?
-// module.exports = (left, numbers) => numbers.map(right => add(left, right))
-
-const R = require('ramda')
-
-const add = R.curry((left, right) => left + right)
-
-module.exports = (left, numbers) => R.map(add(left), numbers)
+// How can we eliminate defining a new function in map using partial application?
+module.exports = (left, numbers) => numbers.map(right => add(left, right))
--- exercises/04-map-add/index.test.js
+++ exercises/04-map-add/index.test.js
@@ -1,13 +1,13 @@
 /* eslint-env jest */
 
-const add = require('./index.js')
+const mapAdd = require('./index.js')
 
-describe('add', () => {
+describe('mapAdd', () => {
   it('adds N to all the numbers in the array', () => {
     const numbers = [1, 2, 3]
     const expected = [6, 7, 8]
     const N = 5
-    const actual = add(N, numbers)
+    const actual = mapAdd(N, numbers)
     expect(actual).toEqual(expected)
   })
 })
