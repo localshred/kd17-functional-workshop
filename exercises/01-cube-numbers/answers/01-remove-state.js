07b7377fdad51662072a218c0675ed837ed98155
BJ Neilsen <bj.neilsen@gmail.com>
2017-11-15 15:22:59 -0600

    Delete unnecessary answer

--- exercises/01-cube-numbers/answers/01-remove-state.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (numbers) {
-  const returnValue = []
-
-  for (let i = 0; i < numbers.length; i++) {
-    returnValue.push(numbers[i] ** 3)
-  }
-
-  return returnValue
-}
